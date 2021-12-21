4e4822609463195252b9096e420daa9298b021ff
Michael Voigt <michael.voigt@visable.com>
2021-12-21 14:45:09 +0100

    Add first version of this GitHub action

---
+++ src/cleanup.js
@@ -0,0 +1,26 @@
+const core = require('@actions/core');
+
+try {
+  const { exec } = require("child_process");
+
+  async function cleanup() {
+    const directory = core.getInput('directory');
+
+    exec(`rm -rf ${directory}`, (error, stdout, stderr) => {
+      if (error) {
+          console.log(`error: ${error.message}`);
+          core.setFailed(error.message)
+          return;
+      }
+      if (stderr) {
+          console.log(`stderr: ${stderr}`);
+          return;
+      }
+      console.log(`stdout: ${stdout}`);
+    });
+  }
+    
+  cleanup();
+} catch (error) {
+  core.setFailed(error.message);
+}
---
+++ src/noop.js
@@ -0,0 +1,2 @@
+console.log("noop - This action will provide a pre & post-run step to clean up the given working directory.");
+
